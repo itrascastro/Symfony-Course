8572f00bf30c30c23ecfccfc43085324cc6b84bd
itrascastro <itrascastro@gmail.com>
2015-03-25 13:22:28 +0100

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="app_default_index")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * addAction
+     *
+     * Description
+     *
+     * @Route("/add/", name="app_default_add")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function addAction()
+    {
+        return $this->render(
+            ':default:form.html.twig',
+            [
+                'title'     => 'Add',
+                'action'    => $this->generateUrl('app_default_doAdd'),
+                'button'    => 'Add',
+            ]
+        );
+    }
+
+    /**
+     * doAdd
+     *
+     * Description
+     *
+     * @Route("/do-add/", name="app_default_doAdd")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function doAdd(Request $request)
+    {
+        $calculator = $this->get('app.service.calculator');
+        $op1 = $request->request->get('op1');
+        $op2 = $request->request->get('op2');
+        $calculator->setOp1($op1);
+        $calculator->setOp2($op2);
+        $calculator->add();
+        $result = $calculator->getResult();
+
+        return $this->render(
+            ':default:result.html.twig',
+            [
+                'op1'       => $op1,
+                'op2'       => $op2,
+                'result'    => $result,
+                'title'     => 'Add Result',
+            ]
+        );
+    }
+}
